3b210649f41c52a4bb68c1330ee8ba7a40bcb63a
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-12-04 15:27:09 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
 function hasCycle(graph) {
+    if (graph.edges.length === 0) {
+        return false;
+    }
+
+    let parent = [];
+    for (let i = 0; i < graph.vertices; i++) {
+        parent[i] = i;
+    }
+
+    function find(i) {
+        while (i !== parent[i]) {
+            i = parent[i];
+        }
+        return i;
+    }
+
+    function union(x, y) {
+        let rootX = find(x);
+        let rootY = find(y);
+        if (rootX !== rootY) {
+            parent[rootY] = rootX;
+        }
+    }
+
+    for (let i = 0; i < graph.edges.length; i++) {
+        let [x, y] = graph.edges[i];
+        if (find(x) === find(y)) {
+            return true;
+        }
+        union(x, y);
+    }
+
     return false;
 }
