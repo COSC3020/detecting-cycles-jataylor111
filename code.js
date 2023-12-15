9f3e2b0d3f5bf7cc4c6481da39c4e9d142427760
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2023-12-14 17:51:44 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,35 +1,4 @@
 function hasCycle(graph) {
-    if (graph.edges.length === 0) {
-        return false;
-    }
-
-    let parent = [];
-    for (let i = 0; i < graph.vertices; i++) {
-        parent[i] = i;
-    }
-
-    function find(i) {
-        while (i !== parent[i]) {
-            i = parent[i];
-        }
-        return i;
-    }
-
-    function union(x, y) {
-        let rootX = find(x);
-        let rootY = find(y);
-        if (rootX !== rootY) {
-            parent[rootY] = rootX;
-        }
-    }
-
-    for (let i = 0; i < graph.edges.length; i++) {
-        let [x, y] = graph.edges[i];
-        if (find(x) === find(y)) {
-            return true;
-        }
-        union(x, y);
-    }
-
+    //Due to techincal issues on my end I was unable to make anymore progress on exercises since monday, but I'm not going to leave this as is, I'd rather it be empty
     return false;
 }
